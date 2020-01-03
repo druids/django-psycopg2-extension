61b1b3ae8dc1477f0132d6324cd5b7b5406b6117
Lubos Matl <lubos.matl@mallpay.cz>
2020-01-03 10:44:39 +0100

    CleanDB uses only vacuum not vacuum full

--- psycopg2_extension/management/commands/cleandb.py
+++ psycopg2_extension/management/commands/cleandb.py
@@ -2,14 +2,14 @@ from psycopg2_extension.base_command import PsycopgBaseCommand
 
 
 class Command(PsycopgBaseCommand):
-    help = 'Clean database (vacuum full and reindex)'
+    help = 'Clean database (vacuum and reindex)'
 
     def handle(self, *args, **options):
         db_object = self._get_db_object(**options)
         conn = self._create_connection(db_object)
 
         with conn.cursor() as cursor:
-            vacuum_query = 'VACUUM FULL ANALYZE'
+            vacuum_query = 'VACUUM ANALYZE'
             self.stdout.write('Running "{}"'.format(vacuum_query))
             cursor.execute(vacuum_query)
             reindex_query = 'REINDEX DATABASE {}'.format(db_object['NAME'])
