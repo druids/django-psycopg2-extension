e5ddc26a9260d358fe93df337ce66d0c0c9b30db
Luboš Mátl <matllubos@gmail.com>
2022-01-07 15:38:21 +0100

    0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="django-psycopg2-extension",
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
-    version='0.1.0',
+    version='0.1.1',
     description="Library contains django commands which helps to prepare and manage PostgreSQL database.",
     author='Lubos Matl',
     author_email='matllubos@gmail.com',
