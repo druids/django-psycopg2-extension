184208198c5bfaf4616cec07b87695641a2e1309
Luboš Mátl <matllubos@gmail.com>
2024-01-26 15:11:45 +0100

    DB views support

--- psycopg2_extension/base_command.py
+++ psycopg2_extension/base_command.py
@@ -4,19 +4,22 @@ from django.db import connections
 
 
 class PsycopgBaseCommand(BaseCommand):
-    help = 'Clean database (vacuum full and reindex)'
+    help = "Clean database (vacuum full and reindex)"
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--database',
-            dest='database',
-            help='Database name in django settings',
-            default='default'
+            "--database",
+            dest="database",
+            help="Database name in django settings",
+            default="default",
         )
 
     def _get_connection(self, alias):
         connection = connections[alias]
-        if connection.settings_dict['ENGINE'] not in {'django.db.backends.postgresql',
-                                                      'django.contrib.gis.db.backends.postgis'}:
-            raise CommandError('Unsupported database backend!')
+        if connection.settings_dict["ENGINE"] not in {
+            "django.db.backends.postgresql",
+            "django.db.backends.postgresql_psycopg2",
+            "django.contrib.gis.db.backends.postgis",
+        }:
+            raise CommandError("Unsupported database backend!")
         return connection
--- psycopg2_extension/management/commands/psqlsnapshot.py
+++ psycopg2_extension/management/commands/psqlsnapshot.py
@@ -9,49 +9,57 @@ class Command(PsycopgBaseCommand):
 
     def add_arguments(self, parser):
         super().add_arguments(parser)
+        parser.add_argument("--file", dest="file", help="Snapshot file path")
         parser.add_argument(
-            '--file',
-            dest='file',
-            help='Snapshot file path'
+            "--noinput",
+            "--no-input",
+            action="store_false",
+            dest="interactive",
+            help="Tells Django to NOT prompt the user for input of any kind.",
         )
         parser.add_argument(
-            '--noinput', '--no-input', action='store_false', dest='interactive',
-            help='Tells Django to NOT prompt the user for input of any kind.',
-        )
-        parser.add_argument(
-            '--stdout', action='store_true', dest='write_to_stdout', help='Write snapshot to stdout',
+            "--stdout",
+            action="store_true",
+            dest="write_to_stdout",
+            help="Write snapshot to stdout",
         )
 
     def handle(self, *args, **options):
-        connection = self._get_connection(options['database'])
+        connection = self._get_connection(options["database"])
 
         command = [
-            'pg_dump', 'postgres://{}:{}@{}:{}/{}'.format(
-                connection.settings_dict['USER'],
-                connection.settings_dict['PASSWORD'],
-                connection.settings_dict['HOST'],
-                connection.settings_dict['PORT'],
-                connection.settings_dict['NAME'],
-            ), '--inserts'
+            "pg_dump",
+            "postgres://{}:{}@{}:{}/{}".format(
+                connection.settings_dict["USER"],
+                connection.settings_dict["PASSWORD"],
+                connection.settings_dict["HOST"],
+                connection.settings_dict["PORT"],
+                connection.settings_dict["NAME"],
+            ),
+            "--inserts",
         ]
 
-        if options['write_to_stdout']:
-            self.stdout.write(subprocess.check_output(command).decode('utf-8'))
+        if options["write_to_stdout"]:
+            self.stdout.write(subprocess.check_output(command).decode("utf-8"))
         else:
-            file_path = options.get('file') or connection.settings_dict.get('SNAPSHOT_FILE')
+            file_path = options.get("file") or connection.settings_dict.get(
+                "SNAPSHOT_FILE"
+            )
 
-            if options['interactive']:
+            if options["interactive"]:
                 message = (
                     f"This will overwrite existing file '{file_path}'!\n"
-                    'Are you sure you want to do this?\n\n'
+                    "Are you sure you want to do this?\n\n"
                     "Type 'yes' to continue, or 'no' to cancel: "
                 )
-                if input(message) != 'yes':
+                if input(message) != "yes":
                     raise CommandError("SQL snapshot cancelled.")
 
             if not file_path:
-                raise CommandError('You must set snapshot file')
+                raise CommandError("You must set snapshot file")
 
-            self.stdout.write(f'Take a snapshot of database "{options["database"]}" to "{file_path}"')
-            with open(file_path, 'w') as file:
+            self.stdout.write(
+                f'Take a snapshot of database "{options["database"]}" to "{file_path}"'
+            )
+            with open(file_path, "w") as file:
                 subprocess.call(command, stdout=file)
